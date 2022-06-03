b89d7b15eb2ca0116f326d9417e8a73ca49a0f63
GianlucaGallone <cloud.gianluca@gmail.com>
2022-06-04 01:06:11 +0200

    aggiunto button con funzione dark/light theme

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var checkbox = document.querySelector('input[name=theme]');
+
+checkbox.addEventListener('change', function() {
+   if(this.checked) {
+      document.documentElement.setAttribute('select-theme', 'dark');
+   } else {
+      document.documentElement.setAttribute('select-theme', 'light');
+   }
+});
